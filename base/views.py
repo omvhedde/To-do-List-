03dfba55b695c7f76d75e191afe815dbf4b07360
omvhedde <prasad8598@gmail.com>
2022-09-09 11:30:46 +0530

    Added user registration page

--- base/views.py
+++ base/views.py
@@ -1,15 +1,19 @@
 from distutils.log import Log
+from locale import T_FMT_AMPM
+from pickle import TRUE
 from typing import List
 from django.shortcuts import render
 from django.views.generic.list import  ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView 
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from .models import Task
 
@@ -22,6 +26,19 @@ class CustomLoginView(LoginView):
     
     def get_success_url(self) -> str:
         return reverse_lazy('tasks')
+
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm  
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
